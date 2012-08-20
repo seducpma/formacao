32879dd5c09487354fc6bdb384b1844857c8d328
Manutencao <alexandrejr_soares@yahoo.com.br>
2012-08-20 08:13:39 -0300

    All

--- app/models/participante.rb
+++ app/models/participante.rb
@@ -7,6 +7,7 @@ class Participante < ActiveRecord::Base
   validates_presence_of :matricula, :if => :tipo? , :message => ' => É PREENCHIMENTO OBRIGATÓRIO'
   validates_presence_of :unidade_id, :if => :tipo? , :message => " É PREENCHIMENTO OBRIGATÓRIO"
   validates_presence_of     :email
+  validates_uniqueness_of :matricula
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   #validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
